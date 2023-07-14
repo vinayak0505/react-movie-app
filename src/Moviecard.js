9bce5567efb03b50b1e006b7a15feaa6bf8cf992
Vinayak Agarwal <vinayakaggarwal05@gmail.com>
2023-07-14 20:50:08 +0530

    ui fixes for stars

--- src/Moviecard.js
+++ src/Moviecard.js
@@ -1,12 +1,12 @@
 import React from "react";
 
-class Moviecard extends React.Component{
-    render(){
+class Moviecard extends React.Component {
+    render() {
         return (
             <div className="main">
                 <div className="movie-card">
                     <div className="left">
-                        <img alt="Poster"/>
+                        <img alt="Poster" src="https://w0.peakpx.com/wallpaper/787/97/HD-wallpaper-avengers-endgame-avengers-endgame.jpg" />
                     </div>
                     <div className="right">
                         <div className="title">The Avenger</div>
@@ -15,7 +15,12 @@ class Moviecard extends React.Component{
 
                         <div className="footer">
                             <div className="rating">8.9</div>
-                            <div className="stars">star</div>
+                            <div className="star-dis">
+                                    <img className="str-btn" alt="decrease" src="https://cdn-icons-png.flaticon.com/128/2801/2801932.png"/>
+                                    <img className="stars" alt="star" src="https://cdn-icons-png.flaticon.com/128/1828/1828884.png"/>
+                                    <img className="str-btn" alt="increase" src="https://cdn-icons-png.flaticon.com/128/1828/1828925.png"/>
+                                    <span>0</span>
+                            </div>
                             <button className="favourite-btn">favourite</button>
                             <button className="card-btn">Add to Cart</button>
                         </div>

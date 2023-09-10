f2840c6447555ec8ec51497b49aee4de0f0cc56a
Vinayak Agarwal <vinayakaggarwal05@gmail.com>
2023-09-10 23:34:51 +0530

    added star option

--- src/Moviecard.js
+++ src/Moviecard.js
@@ -1,7 +1,32 @@
 import React from "react";
 
 class Moviecard extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            title: "The Avenger",
+            plot: "Natureal power",
+            price: "Rs. 199",
+            rating: "8.9",
+            stars: 0,
+        }
+    }
+    addStars = () => {
+        console.log("add");
+        this.setState({
+            stars: this.state.stars  + 1
+        });
+    }
+
+    subStars = () => {
+        console.log("sub");
+        this.setState({
+            stars: this.state.stars  - 1
+        });
+    }
+
     render() {
+        const { title, plot, price, rating,stars } = this.state;
         return (
             <div className="main">
                 <div className="movie-card">
@@ -9,17 +34,17 @@ class Moviecard extends React.Component {
                         <img alt="Poster" src="https://w0.peakpx.com/wallpaper/787/97/HD-wallpaper-avengers-endgame-avengers-endgame.jpg" />
                     </div>
                     <div className="right">
-                        <div className="title">The Avenger</div>
-                        <div className="plot">natureal power</div>
-                        <div className="price">Rs. 199</div>
+                        <div className="title">{title}</div>
+                        <div className="plot">{plot}</div>
+                        <div className="price">{price}</div>
 
                         <div className="footer">
-                            <div className="rating">8.9</div>
+                            <div className="rating">{rating}</div>
                             <div className="star-dis">
-                                    <img className="str-btn" alt="decrease" src="https://cdn-icons-png.flaticon.com/128/2801/2801932.png"/>
-                                    <img className="stars" alt="star" src="https://cdn-icons-png.flaticon.com/128/1828/1828884.png"/>
-                                    <img className="str-btn" alt="increase" src="https://cdn-icons-png.flaticon.com/128/1828/1828925.png"/>
-                                    <span>0</span>
+                                <img className="str-btn" alt="decrease" src="https://cdn-icons-png.flaticon.com/128/2801/2801932.png" onClick={this.subStars} />
+                                <img className="stars" alt="star" src="https://cdn-icons-png.flaticon.com/128/1828/1828884.png" />
+                                <img className="str-btn" alt="increase" src="https://cdn-icons-png.flaticon.com/128/1828/1828925.png" onClick={this.addStars} />
+                                <span>{stars}</span>
                             </div>
                             <button className="favourite-btn">favourite</button>
                             <button className="card-btn">Add to Cart</button>
